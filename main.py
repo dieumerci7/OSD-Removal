1dfc50538e215ad5db731066ddef59426dbf93f3
KyloRen1 <ivanyuk.bogdan1999@gmail.com>
2023-09-01 16:54:36 +0200

    added eda notebook

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
+import cv2
 import click
 
+
+def read_video(path:str) -> list:
+    video_cap = cv2.VideoCapture(path)
+
+    if not video_cap.isOpened():
+        raise ValueError("Could not open video file")
+
+    frames = list()
+    while True:
+        ret, frame = video_cap.read()
+        if not ret:
+            break 
+        frames.append(frame)
+    return frames
+
+
 @click.command(help="")
-@click.option("--cfg", type=str, help="config file path")
-def main():
-    pass
+@click.option("--video-path", type=str, help="testing video path")
+def main(video_path):
+
+    video = read_video(video_path)
+
+
 
 if __name__ == '__main__':
     main()
